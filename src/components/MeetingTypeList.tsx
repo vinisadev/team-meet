ce063460d5e96fafc40b64a52b74ef2a564c0e0b
Vincenzo Fehring <vinfehring@gmail.com>
2025-04-12 23:29:12 -0500

    Setup and style toast notifications

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
 import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { useToast } from "@/hooks/use-toast"
 
 const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
@@ -20,11 +21,17 @@ const MeetingTypeList = () => {
     link: ''
   })
   const [callDetails, setCallDetails] = useState<Call>()
+  const { toast } = useToast()
 
   const createMeeting = async () => {
     if (!client || !user) return
 
     try {
+      if (!values.dateTime) {
+        toast({ title: "Please select a date and time" })
+        return
+      }
+
       const id = crypto.randomUUID()
       const call = client.call('default', id)
 
@@ -47,8 +54,13 @@ const MeetingTypeList = () => {
       if (!values.description) {
         router.push(`/meeting/${call.id}`)
       }
+
+      toast({ title: "Meeting Created" })
     } catch (error) {
       console.log(error)
+      toast({
+        title: "Failed to create meeting"
+      })
     }
   }
 
