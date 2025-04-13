69c7ab273d1720590b2968604354043a100aad08
Vincenzo Fehring <vinfehring@gmail.com>
2025-04-13 00:31:23 -0500

    Some QOL updates

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next'
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Team Meet',
+  description: 'Video Conferencing',
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+}
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils'
-import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { CallControls, CallingState, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 import {
   DropdownMenu,
@@ -12,6 +12,7 @@ import {
 import { LayoutList, Users } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
+import Loader from './Loader'
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 
@@ -20,6 +21,10 @@ const MeetingRoom = () => {
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
   const [showParticipants, setShowParticipants] = useState(false)
   const isPersonalRoom = !!searchParams.get('personal')
+  const { useCallCallingState } = useCallStateHooks()
+  const callingState = useCallCallingState()
+
+  if (callingState !== CallingState.JOINED) return <Loader />
 
   const CallLayout = () => {
     switch (layout) {
@@ -43,7 +48,7 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
         <CallControls />
         <DropdownMenu>
           <div className="flex items-center">
